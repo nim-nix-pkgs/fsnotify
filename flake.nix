449b93e80607b40965bded7f71b2bc0ae53d904a
hugosenari <hugosenari@gmail.com>
2022-03-12 02:27:25 -0300

    chore: re index fsnotify

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A file system monitor in Nim.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."fsnotify-master".url = "path:./master";
+  inputs."fsnotify-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
