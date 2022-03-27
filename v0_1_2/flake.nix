c47cf9cadd4dc08e9d0cb9452507513275630651
hugosenari <hugosenari@gmail.com>
2022-03-27 18:16:18 -0300

    feat(nimpkgs): remove lock

--- v0_1_2/flake.nix
+++ v0_1_2/flake.nix
@@ -7,20 +7,12 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-fsnotify-master.flake = false;
-  inputs.src-fsnotify-master.ref   = "refs/heads/master";
-  inputs.src-fsnotify-master.owner = "planety";
-  inputs.src-fsnotify-master.repo  = "fsnotify";
-  inputs.src-fsnotify-master.dir   = "";
-  inputs.src-fsnotify-master.type  = "github";
-  
-  inputs."timerwheel".owner = "nim-nix-pkgs";
-  inputs."timerwheel".ref   = "master";
-  inputs."timerwheel".repo  = "timerwheel";
-  inputs."timerwheel".dir   = "v0_1_2";
-  inputs."timerwheel".type  = "github";
-  inputs."timerwheel".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."timerwheel".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  inputs.src-fsnotify-v0_1_2.flake = false;
+  inputs.src-fsnotify-v0_1_2.ref   = "refs/tags/v0.1.2";
+  inputs.src-fsnotify-v0_1_2.owner = "planety";
+  inputs.src-fsnotify-v0_1_2.repo  = "fsnotify";
+  inputs.src-fsnotify-v0_1_2.dir   = "";
+  inputs.src-fsnotify-v0_1_2.type  = "github";
   
   inputs."xio".owner = "nim-nix-pkgs";
   inputs."xio".ref   = "master";
@@ -33,13 +25,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-fsnotify-master"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-fsnotify-v0_1_2"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-fsnotify-master";
+    src  = deps."src-fsnotify-v0_1_2";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
