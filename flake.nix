99624fcbe82b13bbfe583457bf3cad8114523521
hugosenari <hugosenari@gmail.com>
2022-03-28 03:02:00 -0300

    chore: re index fsnotify

--- flake.nix
+++ flake.nix
@@ -23,6 +23,14 @@
   inputs."fsnotify-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
   inputs."fsnotify-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."fsnotify-v0_1_4".dir   = "v0_1_4";
+  inputs."fsnotify-v0_1_4".owner = "nim-nix-pkgs";
+  inputs."fsnotify-v0_1_4".ref   = "master";
+  inputs."fsnotify-v0_1_4".repo  = "fsnotify";
+  inputs."fsnotify-v0_1_4".type  = "github";
+  inputs."fsnotify-v0_1_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify-v0_1_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
