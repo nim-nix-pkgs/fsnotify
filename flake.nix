2b11a4ec9d3e37411bed445c9af7c91eec8d5108
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:44 -0300

    chore: re index fsnotify

--- flake.nix
+++ flake.nix
@@ -7,6 +7,30 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."fsnotify-master".dir   = "master";
+  inputs."fsnotify-master".owner = "nim-nix-pkgs";
+  inputs."fsnotify-master".ref   = "master";
+  inputs."fsnotify-master".repo  = "fsnotify";
+  inputs."fsnotify-master".type  = "github";
+  inputs."fsnotify-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fsnotify-v0_1_2".dir   = "v0_1_2";
+  inputs."fsnotify-v0_1_2".owner = "nim-nix-pkgs";
+  inputs."fsnotify-v0_1_2".ref   = "master";
+  inputs."fsnotify-v0_1_2".repo  = "fsnotify";
+  inputs."fsnotify-v0_1_2".type  = "github";
+  inputs."fsnotify-v0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify-v0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fsnotify-v0_1_4".dir   = "v0_1_4";
+  inputs."fsnotify-v0_1_4".owner = "nim-nix-pkgs";
+  inputs."fsnotify-v0_1_4".ref   = "master";
+  inputs."fsnotify-v0_1_4".repo  = "fsnotify";
+  inputs."fsnotify-v0_1_4".type  = "github";
+  inputs."fsnotify-v0_1_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fsnotify-v0_1_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
